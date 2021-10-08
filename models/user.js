93eea6ee56ff24c8bc321f02d519b596f1d4b408
Sang Tran <sang@local>
2021-10-08 16:31:40 +0700

    Create new users, view all users

--- models/user.js
+++ models/user.js
@@ -1,9 +1,18 @@
 const db = reqlib('utils/redis.js')
+const uuid = require('uuid');
 
 class User {
     static listAll = async () => {
-        let all = db.keys('user:*')
-        return all
+        const keys = await db.keys(`user:*`)
+        return await Promise.all(keys.map((key) => new Promise((resolve) => {
+            resolve(db.hgetall(key))
+        })))
+    }
+    static add = async (data) => {
+        data['credit_limit'] = parseFloat(data['credit_limit']) || 0
+        data['id'] = uuid.v4()
+        await db.hmset(`user:${data.id}`, data)
+        return data
     }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var User = reqlib('/models/user.js')
-/* GET users listing. */
+
 router.get('/', async function (req, res) {
     let items = await User.listAll();
     res.send({
-        'data': items
+        'data': items,
+        'count': items.length
     });
+});
+
+router.post('/', async function (req, res) {
+    let user = await User.add(req.body || {})
+    res.send(user)
 });
 
 module.exports = router;
